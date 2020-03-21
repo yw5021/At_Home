efd70770a58f799482297a898dafd95447717af2
yw5021 <yw5021@naver.com>
2020-03-21 16:11:29 +0900

    카드 여러장을 선택해서 손에서 버리는 기능 및 카드 선택 클래스 구현

--- Assets/Scripts/CardSelect.cs
+++ Assets/Scripts/CardSelect.cs
@@ -8,75 +8,107 @@ public class CardSelect : MonoBehaviour {
     List<Card> card_list;
 
     public Button confirm_but;
+    public Button[] select_card_but_arr;
+    public Button[] deselect_card_but_arr;
 
-    int[] select_card_num_arr;
+    List<int> select_card_num_list;
     int select_card_cnt;
 
     int now_select_card_cnt = 0;
 
     void card_image_insert()
     {
-        Debug.Log("실행 되었다");
-    }
-    
-    public void test()
-    {
-        but_func_insert(card_image_insert);
+        //카드 선택창이 켜질때 이미지값 조정  
+        
     }
 
+    public void test_start()
+    {
+        card_select_start(test_result, 3);
+
+        for(int i = 0; i < select_card_but_arr.Length; i++)
+        {
+            select_card_but_arr[i].gameObject.SetActive(true);
+        }
+        for (int j = 0; j < select_card_but_arr.Length; j++)
+        {
+            deselect_card_but_arr[j].gameObject.SetActive(false);
+        }
+
+    }
+
+    void test_result()
+    {
+        string temp_string = "선택된 카드 : ";
+
+        for(int i = 0; i < select_card_num_list.Count; i++)
+        {
+            temp_string += select_card_num_list[i] + " ";
+        }
+        Debug.Log(temp_string);
+    }
+
+    #region 선택 완료 버튼 함수
     void delete_listener()
     {
         confirm_but.onClick.RemoveAllListeners();
     }
 
-    public void but_func_insert(System.Action func)
+    void but_func_insert(System.Action func)
     {
         //버튼에 받은 함수포인터로 클릭 이벤트 지정
         confirm_but.onClick.AddListener(() => func());
         confirm_but.onClick.AddListener(() => delete_listener());
     }
+    #endregion
 
-    void card_select_start(int card_cnt)
+    public void card_select_start(System.Action func, int card_cnt)
     {
-        //뽑을 카드에 대한 정보 ~~~
+        //시작할 때 데이터값 정리
         select_card_cnt = card_cnt;
 
-        select_card_num_arr = new int[select_card_cnt];
+        //select_card_num_arr = new int[select_card_cnt];
+        select_card_num_list = new List<int>();
+
+        //선택 완료 버튼에 이벤트 넣어줌
+        but_func_insert(func);
     }
 
-    public void select_card(int select_num)
+    public void select_card(int select_card_num)
     {
-        //ui 쪽은 바깥에서 건드리는걸로
-        if(now_select_card_cnt > select_card_cnt)
+        Debug.Log(select_card_num + "번 선택됨");
+        if (now_select_card_cnt >= select_card_cnt)
         {
             Debug.Log("error 72157 - 더 이상 선택할 수 없음");
             return;
         }
-
-        select_card_num_arr[now_select_card_cnt] = select_num;
+        select_card_num_list.Add(select_card_num);
 
         now_select_card_cnt++;
+
+        deselect_card_but_arr[select_card_num].gameObject.SetActive(true);
+        select_card_but_arr[select_card_num].gameObject.SetActive(false);
     }
     
-    public void deselect_card(int select_num)
+    public void deselect_card(int select_card_num)
     {
-        if(now_select_card_cnt <= 0)
+        Debug.Log(select_card_num + "번 선택취소됨");
+        if (now_select_card_cnt <= 0)
         {
             Debug.Log("error 98321 - 취소할 대상이 없음");
             return;
         }
 
-        select_card_num_arr[now_select_card_cnt] = -1;
-
         now_select_card_cnt--;
+
+        select_card_num_list.Remove(select_card_num);
+
+        deselect_card_but_arr[select_card_num].gameObject.SetActive(false);
+        select_card_but_arr[select_card_num].gameObject.SetActive(true);
     }
 
-    public int[] return_select_card_num_arr()
+    public List<int> return_select_card_num_list()
     {
-        int[] temp_arr = select_card_num_arr;
-
-        select_card_num_arr = new int[0];
-
-        return temp_arr;
+        return select_card_num_list;
     }
 }
